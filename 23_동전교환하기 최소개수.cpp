260fe57708f7b1f7c8af09069b6b4e4f2da5c24f
yuna61 <yunajoayo@gmail.com>
2025-12-02 21:06:28 +0900

    refactor: 23, 26
    코드 최적화

--- 23_동전교환하기 최소개수.cpp
+++ 23_동전교환하기 최소개수.cpp
@@ -2,38 +2,30 @@
 
 using namespace std;
 
-
-
 int main() {
-	int test_case;
-	cin >> test_case;
-	while (test_case--) {
-		int n, m;
-		cin >> n >> m;
-		int coins[11];
-		int path[1001] = { 0 };
-		int lastCoin[1001] = { 0 };
-		for (int i = 0; i < m;i ++) {
-			cin >> coins[i];
-		}
-		
-		for (int i = 1; i <= n;i++) {
-			int minCoins, newCoin;
-			minCoins = i;
-			newCoin = 1;
-			for (int j = 0; j < m;j++) {
-				if (coins[j] > i) {
-					continue;
-				}
-				if (path[i - coins[j]] + 1 < minCoins) {
-					minCoins = path[i - coins[j]] + 1;
-					newCoin = coins[j];
-				}
-			}
-			path[i] = minCoins;
-			lastCoin[i] = newCoin;
-		}
-		cout << path[n] << "\n";
-
-	}
+    int test_case;
+    cin >> test_case;
+    while (test_case--) {
+        int n, m;
+        cin >> n >> m;
+        int coins[11];
+        int path[1001] = { 0 };
+        int lastCoin[1001] = { 0 };
+        for (int i = 0;i < m;i++) {
+            cin >> coins[i];
+        }
+        for (int i = 1;i <= n;i++) {
+            int minCoins = i;
+            for (int j = 0;j < m;j++) {
+                if (coins[j] > i) {
+                    continue;
+                }
+                if (path[i - coins[j]] + 1 < minCoins) {
+                    minCoins = path[i - coins[j]] + 1;
+                }
+            }
+            path[i] = minCoins;
+        }
+        cout << path[n] << "\n";
+    }
 }
--- 26_사이클검출.cpp
+++ 26_사이클검출.cpp
@@ -1,58 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <set>
+
 using namespace std;
 
 struct UnionFind {
-    vector<int>parent, size;
-    UnionFind(int n) : parent(n), size(n, 0) {
+    vector<int>parent;
+    vector<int>size;
+    UnionFind(int n) :parent(n), size(n, 0) {
         for (int i = 0;i < n;i++) {
             parent[i] = i;
         }
     }
-
     int find(int x) {
         if (parent[x] == x)return x;
         return parent[x] = find(parent[x]);
     }
-
     bool unite(int a, int b) {
         a = find(a);
         b = find(b);
         if (a == b)return false;
         if (size[a] < size[b])swap(a, b);
         parent[b] = a;
-        if (size[a] == size[b])size[a]++;
+        size[a] += size[b];
         return true;
     }
-
 };
 
 int main() {
     int test_case;
     cin >> test_case;
     while (test_case--) {
-        bool hasCycle = false;
         int n, m;
         cin >> n >> m;
+        bool hasCycle = false;
         UnionFind uf(n + 1);
-        set<pair<int, int>>edgeSet;
+
         for (int i = 0;i < m;i++) {
             int u, v;
             cin >> u >> v;
             if (u > v)swap(u, v);
 
-            edgeSet.insert({ u,v });
-
             if (!uf.unite(u, v)) {
                 hasCycle = true;
             }
         }
-        if (hasCycle) {
-            cout << 1 << "\n";
+        if (!hasCycle) {
+            cout << 0 << "\n";
         }
         else {
-            cout << 0 << "\n";
+            cout << 1 << "\n";
         }
 
     }
